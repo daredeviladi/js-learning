ff393cdb170866cb92525413b2c7e727d48e7e4e
Aditya Gupta <112779797+daredeviladi@users.noreply.github.com>
2023-11-08 18:05:41 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,70 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score)); //for finding the datatype of variable
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); //to convert a other datatype to number datatype ///=>when we convert string to number sometimes it give  NaN (always check precisely)
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// null => 0
+// undefined => NaN
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber); //it will give string datatype
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);// hello hitesh
+
+// console.log("1" + 2);        //12
+// console.log(1 + "2");        //12
+// console.log("1" + 2 + 2);    //122
+// console.log(1 + 2 + "2");    //32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
