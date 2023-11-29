9699ea45b46e2d5daedf37c71d0ee830f132fca6
Aditya Gupta <112779797+daredeviladi@users.noreply.github.com>
2023-11-29 17:20:16 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -47,8 +47,39 @@ const users = [         //jab apis ke through array mein objects aaegee unka for
 ]
 
 users[1].email
-console.log(tinderUser  )
+// console.log(tinderUser  )
 
-console.log(Object.keys(tinderUser)); //for finding all the keys
-console.log(Object.values(tinderUser)); //for finding all the values
-console.log(Object.entries(tinderUser)); //for finding all the entries
+// console.log(Object.keys(tinderUser)); //for finding all the keys
+// console.log(Object.values(tinderUser)); //for finding all the values
+// console.log(Object.entries(tinderUser)); //for finding all the entries
+
+
+//destructuring the objects
+const course={
+    coursename : "js in hindi",
+    price:"999",
+    courseInst:"hitesh"
+}
+
+// course.courseInst for accesing the value
+
+const {courseInst :inst}=course;
+// console.log(courseInst);
+// console.log(inst); both will give same output
+
+//in react we use props but to write kless code we destructre code
+// const navbar =({inst})=>{
+
+// }
+
+//API ->apna kam kisi or ke sar dalna...pehle data aata the xml form abhi aata json format mein
+
+//json format - object naam hota haii agar object ka naam nahi hoga to use json ki tarah treat karege . keys,values both are in string
+
+// {
+//     name:"Adi",
+//     coursenamec:"js",
+//     proice: "free"
+// }
+
+
